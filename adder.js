2b35c0b2704e53960df9a96cb5c0f4f45f9162ab
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-23 12:39:09 -0500

    cleanup

--- adder.js
+++ adder.js
@@ -1,6 +1,7 @@
 function adder(a, b) {
+  // validate that both inputs are numbers
   if (!(typeof a === 'number' && typeof b === 'number')) {
-  	throw Error('Oh no!');
+    throw Error('Oh no!');
   }
   return a + b;
 }
--- test/test-adder.js
+++ test/test-adder.js
@@ -2,25 +2,35 @@ const should = require('chai').should();
 
 const adder = require('../adder');
 
+
+// unit tests for our `adder` function
 describe('adder', function() {
 
+  // test the normal case
   it('should add two numbers', function() {
-    const testData = {
-      a: 2,
-      b: 3,
-      expected: 5
-    };
-    const answer = adder(testData.a, testData.b)
-    answer.should.equal(testData.expected);
+    // range of normal inputs, including
+    // notable cases like negative answers
+    const normalCases = [
+      {a: 2, b: 3, expected: 5},
+      {a: 200, b: 2000, expected: 2200},
+      {a: 2, b: -5, expected: -3}
+    ];
+    // for each set of inputs (a, b), `adder` should
+    // produce the expected value
+    normalCases.forEach(function(input) {
+      const answer = adder(input.a, input.b);
+      answer.should.equal(input.expected);
+    });
   });
 
   it('should raise error if args not numbers', function() {
+    // range of bad inputs where not both are numbers
     const badInputs = [
       ['a', 1],
       ['1', 2],
       [2, false]
     ];
-
+    // prove that an error is raised for bad inputs
     badInputs.forEach(function(input) {
       (function() {
           adder(input[0], input[1])
